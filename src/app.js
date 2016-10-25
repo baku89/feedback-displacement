73b614d7bfa437a1c33982c97f97e8469926e573
baku89 <mail@baku89.com>
2016-10-25 14:34:39 -0400

    Make parameters to be saved automatically

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import VueAsyncData from 'vue-async-data'
+import VueLocalStorage from 'vue-localstorage'
 
 Vue.use(VueAsyncData)
+Vue.use(VueLocalStorage)
 
 import Easel from './easel'
 import ImageLoader from './image-loader'
@@ -16,28 +18,34 @@ export default class App extends Vue {
 			el: 'body',
 			data: {
 				effects: [],
-				currentEffect: 'polar-perlin',
+				currentEffect: '',
 				params: {},
 				ui: {
 				}
 			},
-			asyncData: (resolve, reject) => {
+			asyncData: (resolve) => {
 
 				$.getJSON('./assets/effects.json', (data) => {
 					this.effectsData = data
 
 					resolve({
-						effects: _.map(this.effectsData, (e, key) => { return{label: e.label, value: key}}),
-						currentEffect: 'polar-perlin'
+						effects: _.map(this.effectsData, (e, key) => { return{label: e.label, value: key}})
 					})
 
+					this.currentEffect = this.$localStorage.get('currentEffect')
 					this.changeEffect()
+
+					this.$set('params', this.$localStorage.get('currentParams'))
+
 				})
+			},
+			localStorage: {
+				currentEffect: {type: String, default: 'polar-perlin'},
+				currentParams: {type: Object, default: {}}
 			}
 		})
 
 		this.easel = new Easel()
-
 		this.changeEffect = this.changeEffect.bind(this)
 
 		window.Commands.execute('load-source', './assets/default.jpg')
@@ -47,6 +55,7 @@ export default class App extends Vue {
 		for (let key in this.uniforms) {
 			this.uniforms[key].value = this.params[key].value
 		}
+		this.$localStorage.set('currentParams', this.params)
 		this.easel.updateUniforms(this.uniforms)
 	}
 
@@ -56,6 +65,7 @@ export default class App extends Vue {
 		let effect = this.effectsData[this.currentEffect]
 
 		this.$set('params', effect.params)
+		this.$localStorage.set('currentEffect', this.currentEffect)
 
 		let uniforms = {}
 
